e1fd1e749c4113e79b0a59471bdcb24dc34ef6fc
ReidMKelley <rmk2ub@virginia.edu>
2020-02-06 19:09:54 -0500

    Added code in main script to put in an ID number at the front of the file name for easy sequential ordering.
    Also adjusted the file download source to match  the Census's new file locations.

--- CPSDataReader.R
+++ CPSDataReader.R
@@ -11,14 +11,14 @@ source("CPSDataReaderFunctions.R")
 
 
 tic()
-StartMonth = 1
+StartMonth = 8
 StartYear = 2014
 
-EndMonth = 6
+EndMonth = 10
 EndYear = 2015
 Diff = EndYear - StartYear + 1
 MonthsNum = matrix(data = 1:12, nrow = 12, ncol = Diff)
-
+"https://www2.census.gov/programs-surveys/cps/datasets/2016/basic/dec16pub.zip"
 
 if (StartYear <= EndYear) {
   if ((StartYear == EndYear)&&(StartMonth <= EndMonth)) {
@@ -33,19 +33,31 @@ if (StartYear <= EndYear) {
   Years = StartYear:EndYear
   FileDateName = sapply(1:Diff, function(x) str_c(str_to_lower(MonthsName[,x]), str_trunc(Years[x], width = 2, side = "left", ellipsis = "")))  
   FileDateNum = sapply(1:Diff, function(x) as.numeric(str_c(Years[x], str_trunc(str_c("0", MonthsNum[,x]), width = 2, side = "left", ellipsis = ""))))
+  IDMax = Diff*12 - (StartMonth - 1) - (12 - EndMonth)
+  if(IDMax>99) {
+    IDVal = matrix(data = c(rep(NA,StartMonth - 1), str_c("00", 1:9), str_c("0", 10:99), as.character(100:IDMax), rep(NA,12 - EndMonth)), ncol = Diff)
+  } else if ((IDMax<99)&&(IDMax>9)) {
+    IDVal = matrix(data = c(rep(NA,StartMonth - 1), str_c("0", 1:9), as.character(10:IDMax), rep(NA,12 - EndMonth)), ncol = Diff)
+  } else if (IDMax<9) {
+    IDVal = matrix(data = c(rep(NA,StartMonth - 1), as.character(1:IDMax), rep(NA,12 - EndMonth)), ncol = Diff)
+  }
+  
+  
 } else {
   stop("EndYear value is less than StartYear value. Correct before proceeding.")
 }
 
-FileSourcePrefix = "https://thedataweb.rm.census.gov/pub/cps/basic/"
-FilePrefixVals = DataFileLocVal(FileDateNum)
+
+# May want to revert to the old source for this while in testing/development, as it seems to download faster and the old links won't deactiviate till June 2020. Will need to adjust for final use.
+FileSourcePrefix = str_c("https://www2.census.gov/programs-surveys/cps/datasets/", Years, "/basic/")
+
 DictionarySourcePrefix = "https://data.nber.org/data/progs/cps-basic/"
 DictionaryPrefixVals = DataDictionaryLocVal(FileDateNum)
 
 
-FileSourcePaths = sapply(1:Diff, function(x) str_c(FileSourcePrefix, FilePrefixVals[,x], FileDateName[,x], "pub.zip"))
+FileSourcePaths = sapply(1:Diff, function(x) str_c(FileSourcePrefix[x], FileDateName[,x], "pub.zip"))
 DictionarySourcePaths = str_c(DictionarySourcePrefix, DictionaryPrefixVals)
-FileDestinationPath = sapply(1:Diff, function(x) str_c("Z:/Reid/CPSMicrodataReading/MicrodataStorage/", FileDateName[,x], "pub.zip"))
+FileDestinationPath = sapply(1:Diff, function(x) str_c("Z:/Reid/CPSMicrodataReading/MicrodataStorage/", IDVal[,x], FileDateName[,x], "pub.zip"))
 DictionaryDestPath = str_c("Z:/Reid/CPSMicrodataReading/MicrodataStorage/", DictionaryPrefixVals)
 DictionaryUniqueURL = unique(as.vector(DictionarySourcePaths))
 DictionaryUniquePath = unique(as.vector(DictionaryDestPath))
@@ -58,10 +70,10 @@ RemovedFiles = file.remove(FileCleanup)
 
 DictionaryDownloads = sapply(seq_along(DictionaryUniqueURL), function(x) FileDownloader(FileURL = DictionaryUniqueURL[x], FileDest = DictionaryUniquePath[x]))
 FileExtraction = sapply(1:Diff, function(x) sapply(1:12, function(y) FileUnziper(FileToUnzip = FileDestinationPath[y,x], ExtractedFileDest = "C:/Users/Kelley_R/Documents/CPSMicrodataStorage")))
-ExtractedFiles = sapply(1:Diff, function(x) str_c("C:/Users/Kelley_R/Documents/CPSMicrodataStorage/", FileDateName[,x], "pub.dat"))
+ExtractedFiles = sapply(1:Diff, function(x) str_c("C:/Users/Kelley_R/Documents/CPSMicrodataStorage/", IDVal[,x], FileDateName[,x], "pub.dat"))
+FileRenaming = sapply(1:Diff, function(x) file.rename(from = str_c("C:/Users/Kelley_R/Documents/CPSMicrodataStorage/", FileDateName[,x], "pub.dat"), to = ExtractedFiles[,x]))
 
-
-DictionaryMonthConnection = data.frame(FileDateName = as.vector(FileDateName), FileDateNum = as.vector(FileDateNum), FileLocation = as.vector(ExtractedFiles), DataDictionary = as.vector(DictionaryDestPath))
+DictionaryMonthConnection = tibble(OrderNumber = as.vector(IDVal), FileDateName = as.vector(FileDateName), FileDateNum = as.vector(FileDateNum), FileLocation = as.vector(ExtractedFiles), DataDictionary = as.vector(DictionaryDestPath))
 
 
 
